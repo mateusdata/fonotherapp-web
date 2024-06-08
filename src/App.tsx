34e1a81c3671057035b06f0db5ca44e9b0c59eec
Mateus Santos <mateuspele2015@gmail.com>
2024-06-07 21:49:35 -0300

    fix: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 
 import  gif from "./cara_de_assustada.gif"
+import  ImageFlavia from "./assets/flavia.jpeg"
+
+import HeaderPublic from "./components/HeaderPublic"
 import openApp from "./components/openApp"
 export default function App() {
   return (
@@ -8,233 +11,7 @@ export default function App() {
 
       <main id="content">
         <div className="overflow-hidden">
-          <header className="flex  bg-cyan-500 flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm py-3 md:py-0">
-            <nav className="max-w-[85rem] w-full mx-auto px-4 md:px-6 lg:px-8" aria-label="Global">
-              <div className="relative md:flex md:items-center md:justify-between">
-                <div className="flex items-center justify-between">
-
-                  <div className="flex gap-1">
-                    <div className="flex-none  w-7" >
-                      <svg className="w-28 h-auto" width="116" height="32" viewBox="0 0 116 32" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-
-                        <path
-                          d="M1 28.9786V15.9786C1 9.35116 6.37258 3.97858 13 3.97858C19.6274 3.97858 25 9.35116 25 15.9786C25 22.606 19.6274 27.9786 13 27.9786H12"
-                          className="stroke-gray-50 dark:stroke-white" stroke="currentColor" stroke-width="2" />
-                        <path
-                          d="M5 28.9786V16.1386C5 11.6319 8.58172 7.97858 13 7.97858C17.4183 7.97858 21 11.6319 21 16.1386C21 20.6452 17.4183 24.2986 13 24.2986H12"
-                          className="stroke-gray-50 dark:stroke-white" stroke="currentColor" stroke-width="2" />
-                        <circle cx="13" cy="16" r="5" fill="currentColor" className="fill-gray-50 dark:fill-white" />
-                      </svg>
-                    </div>
-
-                    <span className="text-white font-medium text-lg">Fonotherapp</span>
-                  </div>
-
-
-                  <div className="md:hidden">
-                    <button type="button"
-                      className="hs-collapse-toggle size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-800 dark:hover:bg-neutral-700"
-                      data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation"
-                      aria-label="Toggle navigation">
-                      <svg className="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
-                        width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <line x1="3" x2="21" y1="6" y2="6" />
-                        <line x1="3" x2="21" y1="12" y2="12" />
-                        <line x1="3" x2="21" y1="18" y2="18" />
-                      </svg>
-                      <svg className="hs-collapse-open:block hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
-                        width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M18 6 6 18" />
-                        <path d="m6 6 12 12" />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-
-                <div id="navbar-collapse-with-animation"
-                  className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block">
-                  <div
-                    className="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-                    <div
-                      className="flex flex-col gap-x-0 mt-5 divide-y divide-dashed divide-gray-200 md:flex-row md:items-center md:justify-end md:gap-x-7 md:mt-0 md:ps-7 md:divide-y-0 md:divide-solid dark:divide-neutral-700">
-                      <a className="font-medium text-gray-50 hover:text-gray-500 py-3 md:py-6 dark:text-neutral-400 dark:hover:text-neutral-500"
-                        href="#" aria-current="page">
-                        Landing
-                      </a>
-
-                      <a className="font-medium text-gray-50 hover:text-gray-500 py-3 md:py-6 dark:text-neutral-400 dark:hover:text-neutral-500"
-                        href="#">
-                        Company
-                      </a>
-
-                      <div
-                        className="hs-dropdown [--strategy:static] md:[--strategy:absolute] [--adaptive:none] md:[--trigger:hover] py-3 md:py-4">
-                        <button type="button"
-                          className="flex items-center w-full text-gray-50 hover:text-gray-400 font-medium dark:text-neutral-400 dark:hover:text-neutral-500">
-                          Resources
-                          <svg className="ms-2 size-2.5 text-gray-500 dark:text-neutral-500" width="16" height="16"
-                            viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                              stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
-                          </svg>
-                        </button>
-
-                        <div
-                          className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] md:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 w-full hidden z-10 top-full start-0 min-w-60 bg-white md:shadow-2xl rounded-lg py-2 md:p-4 dark:bg-neutral-900 dark:divide-neutral-700 before:absolute before:-top-5 before:start-0 before:w-full before:h-5">
-                          <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-                            <div className="flex flex-col mx-1 md:mx-0">
-                              <a className="group flex gap-x-5 text-gray-800 hover:bg-gray-100 rounded-lg p-4 dark:text-neutral-200 dark:hover:bg-neutral-800"
-                                href="#">
-                                <svg className="flex-shrink-0 size-5 mt-1" xmlns="http://www.w3.org/2000/svg" width="24"
-                                  height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                  stroke-linecap="round" stroke-linejoin="round">
-                                  <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-                                  <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
-                                </svg>
-                                <div className="grow">
-                                  <p className="font-medium text-gray-800 dark:text-neutral-200">Support Docs</p>
-                                  <p
-                                    className="text-sm text-gray-500 group-hover:text-gray-800 dark:text-neutral-500 dark:group-hover:text-neutral-200">
-                                    Explore advice and explanations for all of Preline's features.</p>
-                                </div>
-                              </a>
-
-                              <a className="group flex gap-x-5 text-gray-800 hover:bg-gray-100 rounded-lg p-4 dark:text-neutral-200 dark:hover:bg-neutral-800"
-                                href="#">
-                                <svg className="flex-shrink-0 size-5 mt-1" xmlns="http://www.w3.org/2000/svg" width="24"
-                                  height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                  stroke-linecap="round" stroke-linejoin="round">
-                                  <rect width="7" height="7" x="14" y="3" rx="1" />
-                                  <path
-                                    d="M10 21V8a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H3" />
-                                </svg>
-                                <div className="grow">
-                                  <p className="font-medium text-gray-800 dark:text-neutral-200">Integrations</p>
-                                  <p
-                                    className="text-sm text-gray-500 group-hover:text-gray-800 dark:text-neutral-500 dark:group-hover:text-neutral-200">
-                                    Discover the huge range of tools that Preline integrates with.</p>
-                                </div>
-                              </a>
-
-                              <a className="group flex gap-x-5 text-gray-800 hover:bg-gray-100 rounded-lg p-4 dark:text-neutral-200 dark:hover:bg-neutral-800"
-                                href="#">
-                                <svg className="flex-shrink-0 size-5 mt-1" xmlns="http://www.w3.org/2000/svg" width="24"
-                                  height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                  stroke-linecap="round" stroke-linejoin="round">
-                                  <path d="m7 11 2-2-2-2" />
-                                  <path d="M11 13h4" />
-                                  <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
-                                </svg>
-                                <div className="grow">
-                                  <p className="font-medium text-gray-800 dark:text-neutral-200">API Reference</p>
-                                  <p
-                                    className="text-sm text-gray-500 group-hover:text-gray-800 dark:text-neutral-500 dark:group-hover:text-neutral-200">
-                                    Build custom integrations with our first-class API.</p>
-                                </div>
-                              </a>
-                            </div>
-
-                            <div className="flex flex-col mx-1 md:mx-0">
-                              <a className="group flex gap-x-5 text-gray-800 hover:bg-gray-100 rounded-lg p-4 dark:text-neutral-200 dark:hover:bg-neutral-800"
-                                href="#">
-                                <svg className="flex-shrink-0 size-5 mt-1" xmlns="http://www.w3.org/2000/svg" width="24"
-                                  height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                  stroke-linecap="round" stroke-linejoin="round">
-                                  <circle cx="12" cy="12" r="10" />
-                                  <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-                                  <path d="M12 17h.01" />
-                                </svg>
-                                <div className="grow">
-                                  <p className="font-medium text-gray-800 dark:text-neutral-200">Help Center</p>
-                                  <p
-                                    className="text-sm text-gray-500 group-hover:text-gray-800 dark:text-neutral-500 dark:group-hover:text-neutral-200">
-                                    Learn how to install, set up, and use Preline.</p>
-                                </div>
-                              </a>
-
-                              <a className="group flex gap-x-5 text-gray-800 hover:bg-gray-100 rounded-lg p-4 dark:text-neutral-200 dark:hover:bg-neutral-800"
-                                href="#">
-                                <svg className="flex-shrink-0 size-5 mt-1" xmlns="http://www.w3.org/2000/svg" width="24"
-                                  height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                  stroke-linecap="round" stroke-linejoin="round">
-                                  <circle cx="12" cy="12" r="4" />
-                                  <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-4 8" />
-                                </svg>
-                                <div className="grow">
-                                  <p className="font-medium text-gray-800 dark:text-neutral-200">Developer Hub</p>
-                                  <p
-                                    className="text-sm text-gray-500 group-hover:text-gray-800 dark:text-neutral-500 dark:group-hover:text-neutral-200">
-                                    Learn how to integrate or build on top of Preline.</p>
-                                </div>
-                              </a>
-
-                              <a className="group flex gap-x-5 text-gray-800 hover:bg-gray-100 rounded-lg p-4 dark:text-neutral-200 dark:hover:bg-neutral-800"
-                                href="#">
-                                <svg className="flex-shrink-0 size-5 mt-1" xmlns="http://www.w3.org/2000/svg" width="24"
-                                  height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                  stroke-linecap="round" stroke-linejoin="round">
-                                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                                  <circle cx="9" cy="7" r="4" />
-                                  <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-                                  <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                                </svg>
-                                <div className="grow">
-                                  <p className="font-medium text-gray-800 dark:text-neutral-200">Community Forum</p>
-                                  <p
-                                    className="text-sm text-gray-500 group-hover:text-gray-800 dark:text-neutral-500 dark:group-hover:text-neutral-200">
-                                    Learn, share, and connect with other Preline users.</p>
-                                </div>
-                              </a>
-                            </div>
-
-                            <div className="flex flex-col pt-4 md:pt-0 mx-1 md:mx-0">
-                              <span className="text-sm font-semibold uppercase text-gray-800 dark:text-neutral-200">Customer
-                                stories</span>
-
-                              <a className="mt-4 group flex gap-x-5 items-center" href="#">
-                                <img className="size-32 rounded-lg"
-                                  src="https://i0.wp.com/assets.b9.com.br/wp-content/uploads/2015/03/032615googlegif.gif?fit=500%2C228&ssl=1"
-                                  alt="Image Description" />
-                                <div className="grow">
-                                  <p className="text-gray-800 dark:text-neutral-200">Preline Projects has proved to be most
-                                    efficient cloud based project tracking and bug tracking tool.</p>
-                                  <p
-                                    className="mt-3 inline-flex items-center gap-x-2 text-sm font-semibold text-gray-800 dark:text-neutral-200">
-                                    Learn more
-                                    <svg className="flex-shrink-0 size-4 transition ease-in-out group-hover:translate-x-1"
-                                      xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                      fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                      stroke-linejoin="round">
-                                      <path d="m9 18 6-6-6-6" />
-                                    </svg>
-                                  </p>
-                                </div>
-                              </a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-
-                      <a className="font-medium text-gray-50 hover:text-gray-500 py-3 md:py-6 dark:text-neutral-400 dark:hover:text-neutral-500"
-                        href="#">
-                        Login
-                      </a>
-
-                      <div className="pt-3 md:pt-0">
-                        <a className="py-2.5 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-violet-700 disabled:opacity-50 disabled:pointer-events-none"
-                          href="#">
-                          Request demo
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </nav>
-          </header>
+          <HeaderPublic/>
 
 
 
@@ -381,7 +158,7 @@ export default function App() {
                     </li>
                   </ul>
 
-                  <a onClick={openApp} className="mt-5 py-3 px-5 w-full inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-cyan-500 text-white hover:bg-violet-700 disabled:opacity-50 disabled:pointer-events-none"
+                  <a onClick={openApp} className="mt-5 py-3 px-4 w-full inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-cyan-500 text-white hover:bg-violet-700 disabled:opacity-50 disabled:pointer-events-none"
                     href="fonotherapp://">
                     Iniciar
                   </a>
@@ -579,7 +356,7 @@ export default function App() {
             <div className="lg:grid lg:grid-cols-6 lg:gap-8 lg:items-center">
               <div className="hidden lg:block lg:col-span-2">
                 <img className="rounded-xl"
-                  src="https://s4.ezgif.com/tmp/ezgif-4-41eeaa2b86.gif"
+                  src={gif}
                   alt="Image Description" />
               </div>
 
@@ -599,7 +376,7 @@ export default function App() {
                     <div className="flex items-center">
                       <div className="lg:hidden flex-shrink-0">
                         <img className="size-12 rounded-full"
-                          src="https://images.unsplash.com/photo-1671726203390-cdc4354ee2eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+                          src={ImageFlavia}
                           alt="Image Description" />
                       </div>
                       <div className="ms-4 lg:ms-0">
